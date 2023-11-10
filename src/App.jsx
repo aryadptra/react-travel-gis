779aa92fdd04f92ad0843982dfacc868c655224e
Arya Dwi Putra <aryaadwptr@gmail.com>
2023-11-10 17:21:03 +0700

    route login configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 //import Toaster
 import { Toaster } from "react-hot-toast";
+//import routes
+import Routes from "./routes/routes";
 
 function App() {
   return (
     <>
       <Toaster />
+      <Routes />
     </>
   );
 }
