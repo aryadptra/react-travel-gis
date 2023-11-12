8ab24e562df726778b272e8d9bb920e815cd14ca
Arya Dwi Putra <aryaadwptr@gmail.com>
2023-11-12 16:55:17 +0700

    create dashboard private routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -8,11 +8,28 @@ import { Routes, Route } from "react-router-dom";
 //import view Login
 import Login from "../pages/admin/Login.jsx";
 
+//import component private routes
+import PrivateRoute from "./PrivateRoutes";
+import Dashboard from "../pages/admin/Dashboard/Index.jsx";
+
+//import view admin Dashboard
+// import Dashboard from "../pages/admin/dashboard/Index";
+
 function RoutesIndex() {
   return (
     <Routes>
       {/* route "/admin/login" */}
       <Route path="/admin/login" element={<Login />} />
+
+      {/* private route "/admin/dashboard" */}
+      <Route
+        path="/admin/dashboard"
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   );
 }
