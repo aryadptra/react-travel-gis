7e178f6dfbec95d4a237d39114689ed75a1da396
Arya Dwi Putra <aryaadwptr@gmail.com>
2023-11-13 08:20:58 +0700

    create categories view and routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -10,7 +10,9 @@ import Login from "../pages/admin/Login.jsx";
 
 //import component private routes
 import PrivateRoute from "./PrivateRoutes";
-import Dashboard from "../pages/admin/Dashboard/Index.jsx";
+
+import Categories from "../pages/admin/categories/Index.jsx";
+import Dashboard from "../pages/admin/dashboard/Index.jsx";
 
 //import view admin Dashboard
 // import Dashboard from "../pages/admin/dashboard/Index";
@@ -27,6 +29,16 @@ function RoutesIndex() {
         element={
           <PrivateRoute>
             <Dashboard />
+          </PrivateRoute>
+        }
+      />
+
+      {/* private route "/admin/categories" */}
+      <Route
+        path="/admin/categories"
+        element={
+          <PrivateRoute>
+            <Categories />
           </PrivateRoute>
         }
       />
