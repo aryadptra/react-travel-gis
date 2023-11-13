c0518b27fd0c7f0ccec414e2541ec92f6b89b02e
Arya Dwi Putra <aryadptr.business@gmail.com>
2023-11-13 15:15:32 +0700

    show users data

--- src/pages/admin/users/Index.jsx
+++ src/pages/admin/users/Index.jsx
@@ -1,20 +1,101 @@
 //import react
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//import layout admin
+//import layout
 import LayoutAdmin from "../../../layouts/Admin";
 
+//import BASE URL API
+import Api from "../../../api";
+
+//import js cookie
+import Cookies from "js-cookie";
+
 function UsersIndex() {
+  //title page
+  document.title = "Users - Administrator Travel GIS";
+
+  //state posts
+  const [users, setUsers] = useState([]);
+
+  //state currentPage
+  const [currentPage, setCurrentPage] = useState(1);
+
+  //state perPage
+  const [perPage, setPerPage] = useState(0);
+
+  //state total
+  const [total, setTotal] = useState(0);
+
+  //token
+  const token = Cookies.get("token");
+
+  //function "fetchData"
+  const fetchData = async () => {
+    //fetching data from Rest API
+    await Api.get("/api/admin/users", {
+      headers: {
+        //header Bearer + Token
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((response) => {
+      //set data response to state "categories"
+      setUsers(response.data.data.data);
+
+      //set currentPage
+      setCurrentPage(response.data.data.current_page);
+
+      //set perPage
+      setPerPage(response.data.data.per_page);
+
+      //total
+      setTotal(response.data.data.total);
+    });
+  };
+
+  //hook
+  useEffect(() => {
+    //call function "fetchData"
+    fetchData();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <React.Fragment>
       <LayoutAdmin>
         <div className="row mt-4">
-          <div className="col-12">
-            <div className="card border-0 rounded shadow-sm border-top-success">
+          <div className="col-md-12">
+            <div className="card border-0 border-top-success rounded shadow-sm mb-5">
               <div className="card-header">
                 <span className="font-weight-bold">
                   <i className="fa fa-users"></i> USERS
                 </span>
+              </div>
+              <div className="card-body">
+                <div className="table-responsive">
+                  <table className="table table-bordered table-striped table-hovered">
+                    <thead>
+                      <tr>
+                        <th scope="col">No.</th>
+                        <th scope="col">Full Name</th>
+                        <th scope="col">Email Address</th>
+                        <th scope="col">Actions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {users.map((user, index) => (
+                        <tr key={index}>
+                          <td className="text-center">
+                            {++index + (currentPage - 1) * perPage}
+                          </td>
+                          <td>{user.name}</td>
+                          <td>{user.email}</td>
+                          <td className="text-center"></td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
           </div>
