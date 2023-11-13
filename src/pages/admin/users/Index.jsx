366228023b00f2b2c5c359ebf14a7b5325a3263b
Arya Dwi Putra <aryadptr.business@gmail.com>
2023-11-13 15:15:52 +0700

    add search feature on users data

--- src/pages/admin/users/Index.jsx
+++ src/pages/admin/users/Index.jsx
@@ -10,6 +10,9 @@ import Api from "../../../api";
 //import js cookie
 import Cookies from "js-cookie";
 
+//import Link from react router dom
+import { Link } from "react-router-dom";
+
 function UsersIndex() {
   //title page
   document.title = "Users - Administrator Travel GIS";
@@ -29,10 +32,16 @@ function UsersIndex() {
   //token
   const token = Cookies.get("token");
 
+  //state search
+  const [search, setSearch] = useState("");
+
   //function "fetchData"
-  const fetchData = async () => {
+  const fetchData = async (searchData) => {
+    //define variable "searchQuery"
+    const searchQuery = searchData ? searchData : search;
+
     //fetching data from Rest API
-    await Api.get("/api/admin/users", {
+    await Api.get(`/api/admin/users?q=${searchQuery}`, {
       headers: {
         //header Bearer + Token
         Authorization: `Bearer ${token}`,
@@ -60,6 +69,14 @@ function UsersIndex() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  //function "searchHandler"
+  const searchHandlder = (e) => {
+    e.preventDefault();
+
+    //call function "fetchDataPost" with state search
+    fetchData(search);
+  };
+
   return (
     <React.Fragment>
       <LayoutAdmin>
@@ -72,6 +89,26 @@ function UsersIndex() {
                 </span>
               </div>
               <div className="card-body">
+                <form onSubmit={searchHandlder} className="form-group">
+                  <div className="input-group mb-3">
+                    <Link
+                      to="/admin/users/create"
+                      className="btn btn-md btn-success"
+                    >
+                      <i className="fa fa-plus-circle"></i> ADD NEW
+                    </Link>
+                    <input
+                      type="text"
+                      className="form-control"
+                      value={search}
+                      onChange={(e) => setSearch(e.target.value)}
+                      placeholder="search by user name"
+                    />
+                    <button type="submit" className="btn btn-md btn-success">
+                      <i className="fa fa-search"></i> SEARCH
+                    </button>
+                  </div>
+                </form>
                 <div className="table-responsive">
                   <table className="table table-bordered table-striped table-hovered">
                     <thead>
